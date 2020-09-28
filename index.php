44dfa4720827f4304f542e3788df84a4e846614d
Henry Zhang <henryzhang9802@gmail.com>
2020-09-28 12:00:54 +0100

    Add php cache control for wechat app

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+header('Expires: Sun, 01 Jan 2014 00:00:00 GMT');
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
+include ("main.html");
+
+?>
